d5d7e40e231777432a94e65b0753b61785733bbe
David Weber <david.weber@ict.campus.net>
2023-10-25 09:36:10 +0200

    added Colors and Shapes

--- Tetrisproject2.py
+++ Tetrisproject2.py
@@ -12,5 +12,20 @@ GRID_HEIGHT = HEIGHT // CELL_SIZE
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
-CYAN = (0, 255, 255)
+CYAN=(0,255,255)
 ORANGE = (255, 165, 0)
+PURPLE=(128,0,128)
+YELLOW=(255,255,0)
+GREEN=(0,128,0)
+BLUE=(0,0,255)
+
+#Formen des Spiels
+SHAPES= [[1,1,1,1],
+         [1,1],[1,1],
+         [1,1,1],[0,1,0]
+         [1,1,0],[0,1,1]
+         [1,1,1],[0,0,1],
+         [1,1,1],[0,1,1],
+         [1,1,1],[1,0,0]]
+#Farben der Formen 
+COLORS=[CYAN,RED,ORANGE,PURPLE,YELLOW,GREEN,BLUE]
