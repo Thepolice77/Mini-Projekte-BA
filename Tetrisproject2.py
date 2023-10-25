dbdb8aa182a76a56020108288396c685de87bd2f
David Weber <david.weber@ict.campus.net>
2023-10-25 08:58:11 +0200

    deleted one file and readded other file

---
+++ Tetrisproject2.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+import time
+
+# Konstanten für das Spielfeld
+WIDTH, HEIGHT = 300, 600
+CELL_SIZE = 30
+GRID_WIDTH = WIDTH // CELL_SIZE
+GRID_HEIGHT = HEIGHT // CELL_SIZE
+
+# Farben
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+CYAN = (0, 255, 255)
+ORANGE = (255, 165, 0)
